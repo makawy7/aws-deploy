5a6255ab38c4c997a647b2564b322940dcab76e4
makawy7 <roots.of.emotion@gmail.com>
2022-08-03 21:44:26 +0200

    2nd commit

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "Demo1-env.eba-efm3mctw.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
