5a4f2fbc22371f1d8dd5e4d659a5bb635acd6199
makawy7 <roots.of.emotion@gmail.com>
2022-08-04 01:11:15 +0200

    9

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'Demo1-env.eba-efm3mctw.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'udagram.eba-92ub49im.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
